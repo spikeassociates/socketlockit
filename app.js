6d8024f7ccd22de959585f903e9d29ce1f1b1078
Lorida Cito <l.cito@studiosynthesis.biz>
2020-05-02 22:57:17 +0200

    fix vulnerabilities

--- app.js
+++ app.js
@@ -15,55 +15,35 @@ var express = require('express'),
     //sslConfig = require('./ssl-config'),
     port = process.env.PORT || 7000;
 
-//var credentials = {key: sslConfig.privateKey, cert: sslConfig.certificate};
+    //var credentials = {key: sslConfig.privateKey, cert: sslConfig.certificate};
 
-//
-//allowCrossDomain = function(req, res, next) {
-//    res.header('Access-Control-Allow-Origin', 'localhost');
-//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//    if ('OPTIONS' === req.method) {
-//        res.send(200);
-//    } else {
-//        next();
-//    }
-//}
-
-
-app.configure(function () {
-    var config = {
-    "cors_allowed_domains": ["http://localhost","www.huknow.com"],
-    };
-    var corsOptions = {
-    "origin": config.cors_allowed_domains,
+var corsOptions = {
+    "origin": "http://localhost",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "credentials": true
-    };
-    app.use(cors(corsOptions));
-    app.use(express.bodyParser({limit: '500mb'}));
-    app.use(express.methodOverride());
-    app.use(express.cookieParser());
-    app.use(
-        require('express-session')(
-            {
-                name: 'site_cookie',
-                secret: 'secret',
-                resave: false,
-                saveUninitialized: false,
-                cookie: {
-                    maxAge: 14400000
-                }
+};
+
+app.use(cors(corsOptions));
+app.use(
+    require('express-session')(
+        {
+            name: 'site_cookie',
+            secret: 'secret',
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                maxAge: 14400000
             }
+        }
 
-        )
-    );
-    app.use(passport.initialize()); // Add passport initialization
-    app.use(passport.session());    // Add passport initialization
-    app.use(bodyParser.json({limit: '500mb'})); // support json encoded bodies
-    app.use(bodyParser.urlencoded({limit: '500mb', extended: true }));
+    )
+);
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
+app.use(bodyParser.json({limit: '500mb'})); // support json encoded bodies
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true }));
 
-});
 
 
 if (NodeConfig.type=='ssl'){
